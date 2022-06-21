e48447323a132e6a8af7788c74838481c9bb3957
Misbahur-Rashid <misbahurasif3112@gmail.com>
2022-06-21 22:55:22 +0600

    Home Page Slider

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -24,6 +24,18 @@
         height: 500px;
         padding-top: 100px;
     }
+
+    img.slider-img {
+        height: 400px !important;
+    }
+
+    .custom-product {
+        height: 600px;
+    }
+
+    .slider-text {
+        background-color: green !important;
+    }
 </style>
 
 </html>
